3f849fa48be8f496efc325188002dd6828b55daf
Simon Csongor <39891992+TykiMik@users.noreply.github.com>
2020-04-06 15:44:10 +0200

    changes branch-A

--- src/main/java/hu/bme/mit/spaceship/GT4500.java
+++ src/main/java/hu/bme/mit/spaceship/GT4500.java
@@ -86,6 +86,7 @@ public class GT4500 implements SpaceShip {
           firingSuccess = secondaryTorpedoStore.fire(1);
           wasPrimaryFiredLast = false;
         }
+        //modified lines in branch-A
         break;
     }
 
