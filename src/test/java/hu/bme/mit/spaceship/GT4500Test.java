15d58e309509ceb674aeeb8139f95423646e2f6b
Simon Csongor <39891992+TykiMik@users.noreply.github.com>
2020-05-13 22:54:32 +0200

    deleting not needed import

--- src/test/java/hu/bme/mit/spaceship/GT4500Test.java
+++ src/test/java/hu/bme/mit/spaceship/GT4500Test.java
@@ -4,7 +4,6 @@ import static org.junit.jupiter.api.Assertions.assertEquals;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
-import sun.management.Sensor;
 
 import static org.mockito.Mockito.*;
 
